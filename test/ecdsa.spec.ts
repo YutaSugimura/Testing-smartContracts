646918d55a603b5c75f5838a751a0979bb2b152a
YutaSugimura <nextc3m@gmail.com>
2020-06-22 23:51:44 +0900

    Add .vscode

--- test/ecdsa.spec.ts
+++ test/ecdsa.spec.ts
@@ -1,6 +1,6 @@
 import { use, expect } from 'chai';
 import { MockProvider, deployContract, solidity } from 'ethereum-waffle';
-import { BigNumber, utils } from 'ethers';
+import { BigNumber, utils, Signer } from 'ethers';
 import ECDSAMock from '../build/ECDSAMock.json';
 
 use(solidity);
@@ -11,5 +11,20 @@ describe('ECDSA', () => {
 
   beforeEach(async () => {
     contract = await deployContract(wallet, ECDSAMock);
+  });
+
+  it('toEthSignedMessageHash', async() => {
+    const message = 'hello world';
+    const bytes = utils.hashMessage(message);
+  
+    const b = utils.sha256(bytes);
+
+    const sign = await contract.toEthSignedMessageHash(b);
+
+    // const recover = await contract.recover(b, sign);
+
+    const signer = Signer.isSigner('b7fdade05c68a34c5e8be0fcc0e34b25a86c87592845879047eec83c0745f87a');
+
+    console.log({ bytes, sign, signer });
   });
 });
